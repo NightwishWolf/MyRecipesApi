248c930093986ec3b0fbe164baf9aa64f2c808d1
NightwishWolf <60580397+NightwishWolf@users.noreply.github.com>
2022-08-07 10:45:34 +0200

    added GroupsController

---
+++ Controllers/GroupsController.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeApi.Data;
+using RecipeApi.Models;
+
+namespace RecipeApi.Controllers
+{
+    [Route("Groups")]
+    [ApiController]
+    public class GroupsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GroupsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET single group --> api/Groups/id-number
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Group>> GetGroup(int id)
+        {
+            if (_context.Groups == null)
+            {
+                return NotFound();
+            }
+            var group = await _context.Groups.FindAsync(id);
+
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            return group;
+        }
+
+        // POST group 
+        [HttpPost]
+        public async Task<ActionResult<Group>> PostGroup(Group group)
+        {
+            if (_context.Groups == null)
+            {
+                return Problem("Entity set 'GroupContext.Groups'  is null.");
+            }
+            _context.Groups.Add(group);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetGroup), new { id = group.GroupId }, group);
+        }
+
+        // GET all groups
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Group>>> GetAllGroups()
+        {
+            if (_context.Groups == null)
+            {
+                return NotFound();
+            }
+            return await _context.Groups.ToListAsync();
+
+            // return result;
+        }
+
+        // PUT update a group
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGroup(int id, Group group)
+        {
+            if (id != group.GroupId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(group).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GroupExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE delete a group
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGroup(int id)
+        {
+            if (_context.Groups == null)
+            {
+                return NotFound();
+            }
+            var group = await _context.Groups.FindAsync(id);
+            if (group == null)
+            {
+                return NotFound();
+            }
+
+            _context.Groups.Remove(group);
+            await _context.SaveChangesAsync();
+            String info = String.Format(group.GroupName + " has been deleted as a group from the database");
+
+            return Content(info);
+        }
+
+        private bool GroupExists(int id)
+        {
+            return (_context.Groups?.Any(e => e.GroupId == id)).GetValueOrDefault();
+        }
+    }
+}
+
